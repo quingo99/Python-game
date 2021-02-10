86555ba429fd781e4550fe1433813a680353aa6d
Qui Ngo <quingo@mail.weber.edu>
2021-02-10 09:03:53 -0700

    Update pythongame.py

--- pythongame.py
+++ pythongame.py
@@ -1,4 +1,19 @@
 # Qui Ngo
 # Feb 10, 2021
  
- #This is a game to demo functions in python 
+ # This is a game to demo functions in python 
+
+ # Rock Paper Scissors
+  # Rock beats Scissors
+  # Scissors beat paper
+  # Paper beats Rock
+
+  # Pseudocode -->
+  # User selection
+  # Computer Selection -- get info from user
+  # Evaluate the result 
+  # Declare a winner
+import random
+
+  user_selection = input('What is your selection? (Rock, Papers, Scissors): ')
+  print('you choose ', user_selection)
