6c5b187074ee4c84cd0d7cf2ca8ce12198668fed
Qui Ngo <quingo@mail.weber.edu>
2021-02-17 09:28:23 -0700

    Add while loop

--- pythongame.py
+++ pythongame.py
@@ -15,30 +15,41 @@
   # Declare a winner
 import random
 
-possible_choices = 'Rock', 'Paper', 'Scissor'
-user_selection = input('What is your selection? (Rock, Papers, Scissors): ')
-print('you choose:', user_selection)
-computer_selection = random.choices(possible_choices)
-print('compute choose:', str(computer_selection))
+possible_choices = ['Rock', 'Paper', 'Scissor']
+while True: 
+  user_selection = input('What is your selection? (Rock, Papers, Scissors): ')
+  print('you choose:', user_selection)
+  computer_selection = random.choice(possible_choices)
 
-# Evaluate the resul
-if user_selection == computer_selection:
-  print('It is a tie')
-elif user_selection == 'Rock':
-      if (computer_selection =='Scissor'):
-            print("Congratulation you're winner")
-      else :
-            print("Opp! You're lose")
-elif user_selection == "Scissor" :
-      if (computer_selection =='Paper'):
-            print("Congratulation you're winner")
-      else :
-            print("Opp! You're lose")
-elif user_selection == 'Paper':
-      if (computer_selection =='Rock'):
-            print("Congratulation you're winner")
-      else :
-            print("Opp! You're lose")
+  #random.choices is multiple choice. 
+  #computer_selection = "Rock"
+  print('compute choose:', str(computer_selection))
+
+  # Evaluate the resul
+  if user_selection == computer_selection:
+    print('It is a tie')
+  elif user_selection == 'Rock':
+        if (computer_selection =='Scissor'):
+              print("Congratulation you're winner")
+        else :
+              print("Opp! You're lose")
+  elif user_selection == "Scissor" :
+        if (computer_selection =='Paper'):
+              print("Congratulation you're winner")
+        else :
+              print("Opp! You're lose")
+  elif user_selection == 'Paper':
+        if (computer_selection =='Rock'):
+              print("Congratulation you're winner")
+        else :
+              print("Opp! You're lose")
+  play_again = input('Do you want to play again (y/n): ')
+  if play_again == 'n':
+        break
+        #break out of loop
+
+# This is outside of the loop
+print ('Thanks for playing')
 
 
 
